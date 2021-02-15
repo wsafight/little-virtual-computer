c0cd66f5c16615b9280ed5d2591acbfd41cb08e9
ShianWang <984292420@qq.com>
2021-02-15 10:32:17 +0800

    feat: Memory change

---
+++ src/components/Memory.ts
@@ -0,0 +1,24 @@
+import {TOTAL_MEMORY_SIZE} from './MemoryPosition'
+
+export default class Memory {
+  static ram: number[] = []
+
+  static set(address: number, value: number) {
+    if (isNaN(value)) {
+      throw new Error(`tried to write to an invalid value at ${address}`);
+    }
+    if (address < 0 || address >= TOTAL_MEMORY_SIZE) {
+      throw new Error('tried to write to an invalid memory address');
+    }
+    this.ram[address] = value;
+  }
+
+  // Get the value which is stored at a certain address in memory
+  static get(address: number) {
+    if (address < 0 || address >= TOTAL_MEMORY_SIZE) {
+      throw new Error('tried to read from an invalid memory address');
+    }
+    return this.ram[address];
+  }
+
+}
