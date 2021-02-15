666e0e384181893e3b30da9e3e11db38bc884b6d
ShianWang <984292420@qq.com>
2021-02-15 18:12:52 +0800

    feat: programs reset

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,18 @@ import Audio from "./components/Audio";
 import Assembler from "./components/Assembler";
 import SimulatorUI from "./components/SimulatorUI";
 import Simulation from "./components/Simulation";
-import PROGRAMS from "./programs";
 
 export { default as Simulation } from './components/Simulation'
 export { default as SimulatorUI } from './components/SimulatorUI'
 
-export default function start() {
+export default function start(programs: Record<string, string>) {
   CPU.init();
   Display.init();
   Input.init();
   Audio.init();
   Assembler.init();
   SimulatorUI.initScreen(Display.SCREEN_WIDTH, Display.SCREEN_HEIGHT, Display.SCREEN_PIXEL_SCALE);
-  SimulatorUI.initUI(PROGRAMS);
+  SimulatorUI.initUI(programs);
   Simulation.loadProgramAndReset();
 
   // enable audio to work with chrome autoplay policy :'(
